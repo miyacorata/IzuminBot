d630a468f866518ad3c0a69efe060d93223bf9f9
miyacorata <miyano@cg-portal.net>
2018-01-12 19:06:42 +0900

    できた

--- plugins/progress.py
+++ plugins/progress.py
@@ -1,10 +1,15 @@
 # coding: utf-8
 
-
-from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 
-@listen_to('開発')
-def listen_func(message):
-    print()
-    message.send('えらい！')
+@listen_to('進捗')
+def erai(message):
+    message.reply('えらい！')
+
+@listen_to('^(.*)(や(る|り)|する|します)')
+def fight(message,something,tmp):
+    message.reply('{0}やるんだね、頑張って！'.format(something))
+
+@listen_to('つらい')
+def tsurai(message):
+    message.reply('大丈夫、きっとできるよ...！')
